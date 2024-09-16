75d08c91d0db3607dbaf74f0ee2d7cbfdea3b18d
mbp4 <mblanpon@myuax.com>
2024-09-16 13:01:57 +0200

    saludo modificado

--- app/src/main/java/com/example/appgestionderesiduos/MainActivity.kt
+++ app/src/main/java/com/example/appgestionderesiduos/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppGestionDeResiduosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "usuario",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Bienvenido a la app de reciclaje $name!",
         modifier = modifier
     )
 }
