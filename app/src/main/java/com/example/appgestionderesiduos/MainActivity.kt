0007747909054774e4e55ae8a076b0bff397abd9
Sira <sgonzmad@myuax.com>
2024-10-01 13:54:35 +0200

    limpiar imports no utilizados

--- app/src/main/java/com/example/appgestionderesiduos/MainActivity.kt
+++ app/src/main/java/com/example/appgestionderesiduos/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.appgestionderesiduos
 
 import android.content.Intent
-import android.content.res.Resources.Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,7 +14,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
